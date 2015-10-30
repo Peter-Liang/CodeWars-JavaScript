e4963561df718dc476fe3965031f84060b872374
Peter Liang <peter_liang@outlook.com>
2015-10-30 20:35:43 +0800

    This kata is broken.

---
+++ Solutions/Part_1_Evil_Programming_Government_Restrictions.js
@@ -0,0 +1,11 @@
+/**
+ * Part 1: Evil Programming Government Restrictions
+ * http://www.codewars.com/kata/5255892719453db6d2000a23/train/javascript
+ */
+
+var anarchy = function() {
+    var one = true | true;
+    var two = one + one;
+    var three = two + one;
+    return 'I can write numbers like, ' + one + ', ' + two + ', ' + three + '.';
+}
