14d494f741d68b998db31ef7dd2bd05b0d357c05
Peter Liang <peter_liang@outlook.com>
2015-10-30 21:42:43 +0800

    Solved the kata Count IP Addresses. http://www.codewars.com/kata/526989a41034285187000de4/solutions/javascript

---
+++ Solutions/Count_IP_Addresses.js
@@ -0,0 +1,16 @@
+/**
+ * Count IP Addresses
+ * http://www.codewars.com/kata/526989a41034285187000de4/train/javascript
+ */
+
+function ipsBetween(start, end) {
+    var startArr = start.split('.'),
+        endArr = end.split('.'),
+        result = 0,
+        factor = 1;
+    for (var i = startArr.length - 1; i >= 0; i--, factor *= 256) {
+        result += (endArr[i] - startArr[i]) * factor;
+    };
+
+    return result;
+}
