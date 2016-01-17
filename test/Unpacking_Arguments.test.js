0b98464a430e2256de699ef1e63cc1ff9ecad168
Peter Liang <peter_liang@outlook.com>
2016-01-17 12:29:59 +0800

    Updated create kata script

--- test/Unpacking_Arguments.test.js
+++ test/Unpacking_Arguments.test.js
@@ -1,8 +1,8 @@
 var spread = require('../Solutions/Unpacking_Arguments.js')
 require('should');
 
-describe('should work', function() {
-    it('apply', function() {
+describe('Unpacking Arguments', function() {
+    it('should work', function() {
         spread(function(x, y) {
             return x + y
         }, [1, 2]).should.equal(3);
