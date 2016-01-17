e0f43f150b0fd013be221fb2b2f35b90fcc90c06
Peter Liang <peter_liang@outlook.com>
2016-01-17 11:42:06 +0800

    Updated test facilities.

--- test/Unpacking_Arguments.test.js
+++ test/Unpacking_Arguments.test.js
@@ -1,7 +1,7 @@
 var spread = require('../Solutions/Unpacking_Arguments.js')
+require('should');
 
 describe('should work', function() {
-    var should = require('should');
     it('apply', function() {
         spread(function(x, y) {
             return x + y
